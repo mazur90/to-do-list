438883f15cf1acc39b11c6ef71ab67754123055f
Dawid Mazurek <dawid.mazurek90@gmail.com>
2023-05-04 14:16:42 +0200

    added tasks.css and <li> to script.js

--- js/script.js
+++ js/script.js
@@ -15,8 +15,14 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li>
-            ${task.content}
+            <li class="tasks__item">
+           <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+           ${task.done ? "✔" : ""}
+           </button>
+           <span class="tasks-content${ task.done ? "tasks__content--done" : ""}">${task.content}</span>
+           <button class="tasks__button tasks__button--remove js-remove">
+           🗑️
+           </button>
             </li>
             `
             document.querySelector(".js-tasks").innerHTML = htmlString;
